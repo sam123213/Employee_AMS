c401d4046d683da1090be9acdb04f2f5b2fa8559
Patil Kalpesh <156535390+sam123213@users.noreply.github.com>
2025-07-06 00:06:22 +0530

    sam commit

--- src/main/java/com/itp/AMS/controller/AMSController.java
+++ src/main/java/com/itp/AMS/controller/AMSController.java
@@ -1,3 +1,4 @@
+
 package com.itp.AMS.controller;
 
 import java.time.Duration;
--- src/main/java/com/itp/AMS/entity/UserRole.java
+++ src/main/java/com/itp/AMS/entity/UserRole.java
@@ -1,5 +1,9 @@
 package com.itp.AMS.entity;
 public enum UserRole {
+	hr,
+	HR,
+	MANAGER,
+	Manager,
 	Admin,
 	ADMIN,
 	Employee,
